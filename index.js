6af6128d6b463c9cb535202fdaadedcb8a93dd58
cucurbitbaby <cucurbitbaby@163.com>
2019-10-31 22:36:52 +0800

    最傻方式

---
+++ index.js
@@ -0,0 +1,13 @@
+var dom = document.getElementById('root');
+
+var header = document.createElement('div');
+header.innerHTML = 'header'
+dom.append(header);
+
+var header = document.createElement('div');
+header.innerHTML = 'siderbar'
+dom.append(header);
+
+var header = document.createElement('div');
+header.innerHTML = 'content'
+dom.append(header);
