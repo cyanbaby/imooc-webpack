7b90ba707f6e5480b5aa97992328e8015b5f56f4
cyanbaby <l>
2019-11-04 20:16:44 +0800

    4-7 Lazy Loading 懒加载，Chunk 是什么？- 02 lazy finish

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-function getComponent() {
-  return import(/* webpackChunkName:"lodash" */ 'lodash').then(({ default: _ }) => {
-    var element = document.createElement('div');
-    element.innerHTML = _.join(['cyan', 'baby'], '-');
-    return element;
-  });
+// es7异步函数
+async function getComponent() {
+  const { default: _ } = await import(/* webpackChunkName:"lodash" */ 'lodash');
+
+  const element = document.createElement('div');
+  element.innerHTML = _.join(['cyan', 'baby'], '-');
+
+  return element;
+
 }
 
 document.addEventListener('click', () => {
@@ -11,4 +14,21 @@ document.addEventListener('click', () => {
     document.body.appendChild(element);
   })
 })
+
+// 使用promise要@babel/polyfill 如何没有也没有关系  .babelrc的presets @babel/preset-env 中设置了useBuiltIns: 'usage'
+// 新版的babel内置了，会自动注入的
+
+// function getComponent() {
+//   return import(/* webpackChunkName:"lodash" */ 'lodash').then(({ default: _ }) => {
+//     var element = document.createElement('div');
+//     element.innerHTML = _.join(['cyan', 'baby'], '-');
+//     return element;
+//   });
+// }
+
+// document.addEventListener('click', () => {
+//   getComponent().then(element => {
+//     document.body.appendChild(element);
+//   })
+// })
 
