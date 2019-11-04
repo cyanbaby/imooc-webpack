15878a535a909f3aca7d7f2242472e62e595614a
cyanbaby <l>
2019-11-05 00:45:12 +0800

    4-9 CSS finish,这傻逼讲师又对着官网api一顿吹

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -4,7 +4,7 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
 	entry: {
-	  main: './src/index.js'
+    main: './src/index.js'
 	},
 
   module: {
@@ -52,7 +52,8 @@ module.exports = {
   }
 }
 
-// chunkFilename: '[name].chunk.js'
-// entry里面的main.. 都会走output里面的filename配置项; index.js在引入一个style1.css 就会被直接合并到main.js
-// 非入口的chunk模块会走chunkFilename配置项  
+// 多入口js, 希望他们的引入的css文件都打包到一个css文件
 
+// 多入口js, 希望他们的引入的css文件分别打包
+
+// 底层都是用optimization  splitChunks 的 cacheGroups  去官网看啦
